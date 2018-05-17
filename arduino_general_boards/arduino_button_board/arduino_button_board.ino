975aa09aa54cba8baee54edb875c3461a7fc253e
thehoodbuddha <j.konijn@student.utwente.nl>
2018-05-17 20:05:40 +0200

    new function added

--- arduino_general_boards/arduino_button_board/arduino_button_board.ino
+++ arduino_general_boards/arduino_button_board/arduino_button_board.ino
@@ -4,25 +4,27 @@
 
 #include <Adafruit_NeoPixel.h>
 
-#define BUTTON_PIN   6
 #define PIXEL_PIN    6    // Digital IO pin connected to the NeoPixels.
-
 #define PIXEL_COUNT 24
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
-int buttonpins[] = {2, 4, 7, 8, 12};
-int buttonvalues[5] = {};
-char headers[] = {'X', 'Y', 'Z', 'U', 'V'};
-int lightlocations[] = {1, 6, 11, 16, 21};
-int interval = 500;
-unsigned long previousm = 0;
-char c;
 
-//char HEADERS[];
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+int buttonpins[] = {2, 4, 7, 8, 12, 13};
+int buttonvalues[6] = {};
+char headers[] = {'X', 'Y', 'Z', 'U', 'V', 'Q'};
+int lightlocations[] = {0, 5, 10, 14, 19};
+int interval = 500;
+int long previousm;
+int fadeAmount;
+boolean checkpressed = true;
+char command;
 
 void setup() {
-  for (int i = 0; i < 5; i++) {
+
+  for (int i = 0; i < 6; i++) {
     pinMode(buttonpins[i], INPUT);
   }
+
   Serial.begin(9600);
 
   strip.begin();
@@ -30,8 +32,10 @@ void setup() {
 }
 
 void loop() {
-  readvalues();
-  datasender();
+
+  //buttonlightanimation();
+  // readvalues();
+  //datasender();
   communication();
 
   //  delay(1000);
@@ -45,27 +49,24 @@ void loop() {
 
 void communication() {                //currently some testing.
 
-  if (Serial.available() > 0) {
+  while (Serial.available() > 0) {
 
+    int value = Serial.parseInt();
+    char command = Serial.read();
 
-    c = Serial.read();
-
-    if (c == 'R') {
-      setwheelred();
+    if (command == 'R') {
+      readvalues();
+     // buttonlight(255);
+     buttonlightanimation(255);
+      //datasender();
+    }
+    if (command == 'G') {
     }
 
-  } else if (c == 'G') {
-    setwheelgreen();
-
-
-  }
-  else {
-    theaterChaseRainbow(5000);
   }
 }
-
 void readvalues() {
-  for ( int i = 0; i < 5; i++) {
+  for ( int i = 0; i < 6; i++) {
     buttonvalues[i] = digitalRead(buttonpins[i]);
   }
 }
@@ -75,55 +76,77 @@ void readvalues() {
 void datasender() {
   if (millis() - previousm > interval) {
     previousm = millis();
-    for (int z = 0; z < 5; z++) {
+    for (int z = 0; z < 6; z++) {
       Serial.print(headers[z]);
       Serial.println(buttonvalues[z]);
-
     }
   }
 
 }
-void setwheelred() {
-  for ( int i = 0;  i < strip.numPixels(); i++) {
-    strip.setPixelColor(i, 255, 0, 0);
+
+void color_strip(byte red, byte green, byte blue, int brightness) {
+
+  for (int i = 0; i < PIXEL_COUNT;  i++) {
+    strip.setBrightness(brightness);
+    strip.setPixelColor(i, strip.Color(red, green, blue));
+
+
   }
   strip.show();
-
-}
-void setwheelgreen() {
-  for ( int i = 0;  i < strip.numPixels(); i++) {
-    strip.setPixelColor(i, 0, 255, 0);
-  }
-  strip.show();
-
-
 }
 
 
-void buttonlight() {
+void buttonlightanimation(int value) {
+  strip.setBrightness(value);
+
+  if ( buttonvalues[5] == 1 && checkpressed) {
+    for (int i = 0; i < 5; i++) {
+      strip.setPixelColor(lightlocations[i], Wheel(((i * 256 / 5) + i) & 255));
+      strip.show();
+    }
+
+    delay(1000);
+    rainbow(1000);
+    fadeAmount++;
+    color_strip(255, 255, 255, 255);
+    checkpressed = false;
+    //    if ( fadeAmount < 0 || fadeAmount> 255) {
+    //    fadeAmount * -1;
+    //    }
+
+  }
 
 
-  strip.setBrightness(255);
+}
+
+
+
+
+
+
+
+
+
+void buttonlight(int value) {
+
   for (int i = 0; i < 5; i++) {
     if (buttonvalues[i] == 1) {
-
-      strip.setPixelColor(lightlocations[i], 255, 255, 255);
-
-    } else {
-      for (int x = 0; x < strip.numPixels(); x++) {
-        strip.setPixelColor(i, 0, 0, 0);
-      }
-
+      strip.setBrightness(value);
+      strip.setPixelColor(lightlocations[i], Wheel(((i * 256 / 5) + i) & 255));
+      //      strip.setPixelColor(lightlocations[i], 255, 255, 255);
+      strip.show();
     }
-    strip.show();
-
-
+    if (buttonvalues[i] == 0) {
+      strip.setPixelColor(lightlocations[i], 0, 0, 0);
+      strip.show();
+    }
 
   }
-
-
-
+  //strip.show();
 }
+
+
+
 uint32_t Wheel(byte WheelPos) {
   WheelPos = 255 - WheelPos;
   if (WheelPos < 85) {
@@ -140,6 +163,18 @@ uint32_t Wheel(byte WheelPos) {
 
 
 
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+
+  for (i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    strip.show();
+    delay(wait);
+
+  }
+}
+
 void rainbowCycle(uint8_t wait) {
   uint16_t i, j;
 
@@ -151,7 +186,6 @@ void rainbowCycle(uint8_t wait) {
     delay(wait);
   }
 }
-
 
 
 void theaterChaseRainbow(uint8_t wait) {
