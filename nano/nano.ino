84c1a0becc8bd30e8a6d19764d6f7bbc318951f6
thehoodbuddha <j.konijn@student.utwente.nl>
2018-05-31 16:51:13 +0200

    arduino testing new protocol

--- nano/nano.ino
+++ nano/nano.ino
@@ -9,10 +9,15 @@
 #define bluepin  10
 #define greenpin  9
 
-#define NUM_LEDS   28
+#define NUM_LEDS   60
 #define BRIGHTNESS  255
 #define METO_SIZE 8
 
+//#define red   Color(255,0,0)
+//#define yellow   Color(255,255,0)
+//#define green  Color(0,255,0)
+//#define purple  Color(255,0,255)
+//#define blue  Color(0,0,255)
 
 byte rr = 0xff;
 byte gg = 0xff;
@@ -20,16 +25,25 @@ byte bb = 0xff;
 
 int fadeAmount;
 char c;
+char command;
+char command2;
+String data;
+int value;
+String buff = "";
 
+int currentValue = 0;
+int values[3] ;
 int a_RED;
 int a_BLUE;
 
+boolean check =  false;
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, leddatapin, NEO_GRB + NEO_KHZ800);
 
 
 
 void setup() {
+
   Serial.begin(9600);
   strip.begin();
   strip.setBrightness(BRIGHTNESS);
@@ -38,67 +52,37 @@ void setup() {
   pinMode(redpin, OUTPUT);
   pinMode(bluepin, OUTPUT);
   pinMode(greenpin, OUTPUT);
-
+  pinMode(3, OUTPUT);
 
 }
 
 void loop() {
-
-  communication();
-  //   color_strip(255,255,0,255);
-
-
-  //color_strip(255,255,0,255);
+  communication2();
+  control();
 }
 
 
 
-void communication() {                //currently some testing.
+void showStrip() {
+  strip.show();
 
-  while (Serial.available() > 0) {
+}
+void color_strip(byte red, byte green, byte blue, int brightness) {
 
-    int value = Serial.parseInt();
-
-    char command = Serial.read();
-
-    if (command == 'R') {
-      color_strip(255, 0, 0, value);
-    }
-
-    if (command == 'G') {
-      color_strip(0, 255, 0, value);
+  for (int i = 0; i < NUM_LEDS;  i++) {
+    strip.setBrightness(brightness);
+    strip.setPixelColor(i, strip.Color(red, green, blue));
 
 
-    }
-    if (command == 'B') {
-      color_strip(0, 0, 255, value);
-
-
-    }
-    if (command == 'A') {
-      int zz = random(1, 6);
-
-      if ( zz != 5) {
-        rr = 0xff;
-        gg = 0xff;
-        bb = 0xff;
-      }
-      else {
-        rr = 0xff;
-        gg = 0x00;
-        bb = 0x00;
-
-      }
-      meteorRain( rr, gg, bb, METO_SIZE, random(60, 150), true, 50);
-
-    }
   }
+  showStrip();
 }
-void color_rgbstrip() {
 
-  analogWrite(redpin, 255);
-  analogWrite(bluepin, 255);
-  analogWrite(greenpin, 255);
+
+void color_rgbstrip(int r, int b, int g) {
+  analogWrite(redpin, r);
+  analogWrite(bluepin, b);
+  analogWrite(greenpin, g);
 }
 
 void color_angry() {
@@ -123,21 +107,7 @@ void color_angry() {
 
 
 
-void color_strip(byte red, byte green, byte blue, int brightness) {
 
-  for (int i = 0; i < NUM_LEDS;  i++) {
-    strip.setBrightness(brightness);
-    strip.setPixelColor(i, strip.Color(red, green, blue));
-
-
-  }
-  showStrip();
-}
-
-void showStrip() {
-  strip.show();
-
-}
 
 void setPixel(int Pixel, byte red, byte green, byte blue) {
   strip.setPixelColor(Pixel, strip.Color(red, green, blue));
@@ -185,6 +155,91 @@ void fadeToBlack(int ledNo, byte fadeValue) {
 
   strip.setPixelColor(ledNo, r, g, b);
 }
+
+void communication2(){
+while (Serial.available()) {
+    String incomingValue = Serial.readStringUntil('\n');
+    if( incomingValue.length() > 0) {
+//      Serial.println(incomingValue.charAt(0));
+      values[0]= String(incomingValue.charAt(0)).toInt();
+     
+      incomingValue ="";
+      delay(10);
+    }
+}
+if(values[0]==1){
+    analogWrite(3,255);
+}else{
+    analogWrite(3,0);
+}
+
+}
+
+void communication() {
+//       Serial.print("X: ");
+//       Serial.println(values[0]);
+//       Serial.print("Y: ");
+//       Serial.println(values[1]);
+//       Serial.print("Z: ");
+//       Serial.println(values[2]);
+  while (Serial.available()) {
+    String incomingValue = Serial.readStringUntil('\n');
+    if( incomingValue.length() > 0) {
+      Serial.println(incomingValue.charAt(0));
+      values[0]= String(incomingValue.charAt(0)).toInt();
+      values[1]= String(incomingValue.charAt(1)).toInt();
+      values[2]= incomingValue.substring(2,5).toInt();
+      incomingValue ="";
+      delay(10);
+    }
+  }
+}
+void control() {
+
+
+  if (values[0] == 1) {
+  
+    switch (values[1]) {
+      case 1:   //red
+        
+        color_rgbstrip(values[2], 0, 0);              //red
+        break;
+      case 2:
+        color_rgbstrip(values[2], values[2], 0);             //yellow
+        break;
+      case 3:
+        color_rgbstrip(values[2], 0, values[2]);             //purple
+        break;
+      case 4:
+        color_rgbstrip(0, values[2], 0);              // green
+        break;
+      case 5:
+        color_rgbstrip(values[2], 0, 0);               //blue
+        break;
+    }
+    
+    }else{
+      analogWrite(3,0);
+
+  }
+  if (values[2] == 1) {
+    int zz = random(1, 6);
+    if ( zz != 5) {
+      rr = 0xff;
+      gg = 0xff;
+      bb = 0xff;
+    }
+    else {
+      rr = 0xff;
+      gg = 0x00;
+      bb = 0x00;
+
+    }
+    meteorRain( rr, gg, bb, METO_SIZE, random(60, 150), true, 50);
+
+  }
+}
+
 
 
 
