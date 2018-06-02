5855bed80fe2e44582eb7b4ef1637d17168ac139
thehoodbuddha <j.konijn@student.utwente.nl>
2018-06-02 13:55:36 +0200

    code clean-up, also the control function has been improved

--- nano/nano.ino
+++ nano/nano.ino
@@ -13,37 +13,23 @@
 #define BRIGHTNESS  255
 #define METO_SIZE 8
 
-//#define red   Color(255,0,0)
-//#define yellow   Color(255,255,0)
-//#define green  Color(0,255,0)
-//#define purple  Color(255,0,255)
-//#define blue  Color(0,0,255)
+#define red 0xFF0000 // 0x
+#define yellow 0xFFFF00
+#define green 0x008000
+#define purple 0x800080
+#define blue 0x0000FF
 
 byte rr = 0xff;
 byte gg = 0xff;
 byte bb = 0xff;
 
-int fadeAmount;
-char c;
-char command;
-char command2;
-String data;
-int value;
-String buff = "";
-char val; //the character read from processing
-
 int currentValue = 0;
-int values[3] ;
+int values[3];
 
-int a_RED;
 int a_BLUE;
 
-boolean check =  false;
-String inputread;
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, leddatapin, NEO_GRB + NEO_KHZ800);
-
-
 
 void setup() {
 
@@ -58,7 +44,6 @@ void setup() {
   pinMode(3, OUTPUT);
 
 }
-
 void loop() {
   communication();
   control();
@@ -68,15 +53,12 @@ void loop() {
 
 void showStrip() {
   strip.show();
-
 }
-void color_strip(byte red, byte green, byte blue, int brightness) {
+void color_strip(uint32_t  color, int brightness) {
 
   for (int i = 0; i < NUM_LEDS;  i++) {
     strip.setBrightness(brightness);
-    strip.setPixelColor(i, strip.Color(red, green, blue));
-
-
+    strip.setPixelColor(i, color);
   }
   showStrip();
 }
@@ -88,54 +70,24 @@ void color_rgbstrip(int r, int b, int g) {
   analogWrite(greenpin, g);
 }
 
-void color_angry() {
-  fadeAmount++;
-
-  a_BLUE += (fadeAmount * random(0.05, 2));
-  a_RED += (fadeAmount * random(0.05, 2));
-
-  if ( a_BLUE < 0 || a_BLUE > 255) {
-    fadeAmount * -1;
-  }
-  if (a_RED <= 0 || a_RED >= 255) {
-
-    fadeAmount * -1;
-  }
-
-  analogWrite(redpin, a_RED);
-  analogWrite(bluepin, a_BLUE);
-
-
-}
-
-
-
-
-
 void setPixel(int Pixel, byte red, byte green, byte blue) {
   strip.setPixelColor(Pixel, strip.Color(red, green, blue));
 }
 
 void meteorRain(byte red, byte green, byte blue, byte meteorSize, byte meteorTrailDecay, boolean meteorRandomDecay, int SpeedDelay) {
-
-
   for (int i = 0; i < NUM_LEDS + NUM_LEDS; i++) {
-
-
     // fade brightness all LEDs one step
     for (int j = 0; j < NUM_LEDS; j++) {
       if ( (!meteorRandomDecay) || (random(10) > 5) ) {
         fadeToBlack(j, meteorTrailDecay );
       }
     }
-
     // draw meteor
     for (int j = 0; j < meteorSize; j++) {
       if ( ( i - j < NUM_LEDS) && (i - j >= 0) ) {
         setPixel(i - j, red, green, blue);
       }
     }
-
     showStrip();
     delay(SpeedDelay);
   }
@@ -159,40 +111,40 @@ void fadeToBlack(int ledNo, byte fadeValue) {
   strip.setPixelColor(ledNo, r, g, b);
 }
 
-void communication2() {
+//void communication2() {
+//  while (Serial.available()) {
+//    String incomingValue = Serial.readStringUntil('\n');
+//    if ( incomingValue.length() > 0) {
+//      //      Serial.println(incomingValue.charAt(0));
+//      values[0] = String(incomingValue.charAt(0)).toInt();
+//
+//      incomingValue = "";
+//      delay(10);
+//    }
+//  }
+//  if (values[0] == 1) {
+//    analogWrite(3, 255);
+//  } else {
+//    analogWrite(3, 0);
+//  }
+//
+//}
+
+
+/*
   while (Serial.available()) {
     String incomingValue = Serial.readStringUntil('\n');
-    if ( incomingValue.length() > 0) {
-      //      Serial.println(incomingValue.charAt(0));
-      values[0] = String(incomingValue.charAt(0)).toInt();
-
-      incomingValue = "";
+    if( incomingValue.length() > 0) {
+      Serial.println(incomingValue.charAt(0));
+      values[0]= String(incomingValue.charAt(0)).toInt();
+      values[1]= String(incomingValue.charAt(1)).toInt();
+      values[2]= incomingValue.substring(2,5).toInt();
+      incomingValue ="";
       delay(10);
     }
   }
-  if (values[0] == 1) {
-    analogWrite(3, 255);
-  } else {
-    analogWrite(3, 0);
-  }
-
-}
-
+*/
 void communication() {
-  /*
-    while (Serial.available()) {
-      String incomingValue = Serial.readStringUntil('\n');
-      if( incomingValue.length() > 0) {
-        Serial.println(incomingValue.charAt(0));
-        values[0]= String(incomingValue.charAt(0)).toInt();
-        values[1]= String(incomingValue.charAt(1)).toInt();
-        values[2]= incomingValue.substring(2,5).toInt();
-        incomingValue ="";
-        delay(10);
-      }
-    }
-  */
-
   inputread = "";
   if (Serial.available())
   { // If data is available to read,
@@ -209,31 +161,17 @@ void communication() {
     values[0] = String(inputread.charAt(0)).toInt();
     values[1] = String(inputread.charAt(1)).toInt();
     values[2] = inputread.substring(2).toInt(); //from char(2) until the end
-    if (values[0] == 1 && values[1] == 2)
-      analogWrite(3, values[2]);
-    
-    /*
-        Serial.print("X: ");
-        Serial.println(values[0]);
-        Serial.print("Y: ");
-        Serial.println(values[1]);
-        Serial.print("Z: ");
-        Serial.println(values[2]);
-    */
-
-
   }
 
   delay(10); //TODO We need to check if this is actually necessary
 }
+
 void control() {
 
-
-  if (values[0] == 1) {
+  if (values[0] ==1) {
 
     switch (values[1]) {
       case 1:   //red
-
         color_rgbstrip(values[2], 0, 0);              //red
         break;
       case 2:
@@ -250,11 +188,27 @@ void control() {
         break;
     }
 
-  } else {
-    analogWrite(3, 0);
+  } else if ( values[0] == 2) {
 
-  }
-  if (values[2] == 1) {
+    switch (values[1]) {
+      case 1:   
+        color_strip(red, 255);              //red
+        break;
+      case 2:
+        color_strip(yellow, 255);             //yellow
+        break;
+      case 3:
+        color_strip(purple, 255);             //purple
+        break;
+      case 4:
+        color_strip(green, 255);              // green
+        break;
+      case 5:
+        color_strip(blue, 255);              //blue
+        break;
+    }
+  } else if (values[0] == 3) {
+
     int zz = random(1, 6);
     if ( zz != 5) {
       rr = 0xff;
@@ -265,12 +219,11 @@ void control() {
       rr = 0xff;
       gg = 0x00;
       bb = 0x00;
-
     }
     meteorRain( rr, gg, bb, METO_SIZE, random(60, 150), true, 50);
-
   }
 }
+
 
 
 
